4d198a5c097b3cf224d16d33e47379154b7e0a9b
Daniel-U12 <daniel.utvich12@gmail.com>
2019-07-13 00:09:24 -0400

    working pot code for the rigidware pot

--- alpha_c_code/alpha_c_code.ino
+++ alpha_c_code/alpha_c_code.ino
@@ -1,101 +1,28 @@
-int pot1 = 32;
-int pot2 = 31;
-int sldr = 33;
-int but_g = 28;
-int but_val = 0;
-int but_r = 27;
-int switch1 = 26; //ARPG
-int switch2 = 24;
-
-int pot_sldr1 = 23;
-int pot_sldr2 = 22;
-
-float pot1_val = 0;
-float curr_pot1_val = 0;
-float pot2_val = 0;
-float sldr_val = 0;
-
-int pot1_cont = 1;
-int pot2_cont = 2;
-int sldr_cont = 3;
-int pot_sldr1_cont = 4;
-int pot_sldr2_cont = 5;
-int but_cont = 6;
-int sw1_cont = 7;
+int noise_pot = 33;
+float next_noise_val = 0;
+float noise_val = 0;
+float noise_ctrl = 0;
 
 int channel = 1;
-
-int sw1_state = 0;
-float pot_sldr1_val = 0;
 
 void setup() {
 // put your setup code here, to run once:
   Serial.begin(38400);
-  pinMode(pot1, INPUT);
-  //pinMode(pot2, INPUT);
-  pinMode(but_g, INPUT);
-  pinMode(but_r, INPUT);
-  pinMode(switch1,INPUT);
-  //pinMode(switch2, INPUT);
-  //pinMode(pot_sldr1, INPUT);
-  //pinMode(pot_sldr2, INPUT);
-  sw1_state = digitalRead(switch1);
+  pinMode(noise_pot, INPUT);
+
 }
 
 void loop() {
-  curr_pot1_val = de_sketchify(pot1, 6);
-  
-  float temp_pot_val = analogRead(pot_sldr1);
-//  if ( temp_pot_val/8 < pot_sldr1_val -  7 || temp_pot_val/8 > pot_sldr1_val + 7){  
-//    pot_sldr1_val = analogRead(pot_sldr2)/8;
-//    Serial.println(strcat("slder pot 1: ", static_cast<char>(pot_sldr1_val)));
-//    usbMIDI.sendControlChange(pot_sldr1_cont, pot_sldr1_val, channel);
-//  }
+  next_noise_val = de_sketchify(noise_pot, 200);
 
-//  if (analogRead(pot_sldr2)/8 <= 100){
-//    int value = analogRead(pot_sldr2)/8;
-//    Serial.println(value <= 10 ? 0 : value);
-//    usbMIDI.sendControlChange(pot_sldr2_cont, value, channel);
-//  }
-
-  if ( curr_pot1_val - 0.5  >= pot1_val || curr_pot1_val + 0.5 <= pot1_val){
-    ///WORKS PUT POT1 CODE HERE
-    pot1_val = curr_pot1_val;
-    float value = ( pot1_val / 851 ) * 123;
-    Serial.println(pot1_val);
-    usbMIDI.sendControlChange(pot1_cont, value, channel);
+  if ( get_delta(next_noise_val, noise_val) >= 5 ) {
+    noise_val = next_noise_val;
+    usbMIDI.sendControlChange(noise_ctrl, 123*(noise_val/684), channel);
+    Serial.println(noise_val);
   }
 
-//  if ( digitalRead(but_g) == LOW ){
-//    //Serial.println("but g");
-//    //WORKS PUT CODE FOR GREEN BUTTON HERE
-//    but_val += 10;
-//    usbMIDI.sendControlChange(but_cont, but_val, channel);
-//    delay(150);
-//
-//  }
-//  if ( digitalRead(but_r) == LOW ){
-//    //Serial.println("but r");
-//    but_val -= 10;
-//    usbMIDI.sendControlChange(but_cont, but_val, channel);
-//    //WORKS PUT CODE FOR RED BUTTON HERE
-//    delay(150);
-//  }
-//  if (digitalRead(switch1) != sw1_state){
-//    sw1_state = digitalRead(switch1);
-//    Serial.println(digitalRead(switch1));
-//    usbMIDI.sendControlChange(sw1_cont, sw1_state, channel);
-//  }
-//  
-//  float curr_sldr = analogRead(sldr);
-//  if ( curr_sldr < sldr_val -  5|| curr_sldr > sldr_val + 5){
-//    sldr_val = curr_sldr;
-//    float value = ( sldr_val / 90 ) * 250;
-//    Serial.println(value);
-//    usbMIDI.sendControlChange(sldr_cont, value, channel);
-//  }
 
-delay(1000);
+  //delay(1000);
 }
 
 float de_sketchify(int port, int iter){
@@ -107,5 +34,9 @@ float de_sketchify(int port, int iter){
   avg_reading = avg_reading / iter;
   return avg_reading;
 
+}
+
+float get_delta(float next_val, float cur_val){
+  return abs(cur_val - next_val);
 }
 
