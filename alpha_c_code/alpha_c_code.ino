6e8a34c44aacf04258694f3db3e30e12f6bd66df
Daniel-U12 <daniel.utvich12@gmail.com>
2019-07-13 00:46:15 -0400

    button code

--- alpha_c_code/alpha_c_code.ino
+++ alpha_c_code/alpha_c_code.ino
@@ -3,26 +3,58 @@ float next_noise_val = 0;
 float noise_val = 0;
 float noise_ctrl = 0;
 
+int vol_pot = 35;
+float next_vol_val = 0;
+float vol_val = 0;
+float vol_ctrl = 1;
+
+int button_4 = 29;
+int button_3 = 30;
+int button_2 = 31;
+int button_1 = 32;
+int arp_ctrl = 3;
+
+
 int channel = 1;
 
 void setup() {
 // put your setup code here, to run once:
   Serial.begin(38400);
   pinMode(noise_pot, INPUT);
+  pinMode(vol_pot, INPUT);
+  pinMode(button_1, INPUT);
+  pinMode(button_2, INPUT);
+  pinMode(button_3, INPUT);
+  pinMode(button_4, INPUT);
 
 }
 
 void loop() {
   next_noise_val = de_sketchify(noise_pot, 200);
+  next_vol_val = de_sketchify(vol_pot, 10);
 
   if ( get_delta(next_noise_val, noise_val) >= 5 ) {
     noise_val = next_noise_val;
     usbMIDI.sendControlChange(noise_ctrl, 123*(noise_val/684), channel);
-    Serial.println(noise_val);
+    //Serial.println(noise_val);
   }
 
+  if ( get_delta(next_vol_val, vol_val) >= 2.5){
+    vol_val = next_vol_val;
+    usbMIDI.sendControlChange(vol_ctrl, 123*(vol_val/838), channel);
+    //Serial.println(vol_val);
+  }
 
-  //delay(1000);
+  if (digitalRead(29) == LOW){
+    usbMIDI.sendControlChange(arp_ctrl, -10, channel);
+  } else if (digitalRead(30) == LOW){
+    usbMIDI.sendControlChange(arp_ctrl, -20, channel);
+  } else if (digitalRead(31) == LOW){
+    usbMIDI.sendControlChange(arp_ctrl, -30, channel);
+  } else if (digitalRead(32) == LOW){
+    usbMIDI.sendControlChange(arp_ctrl, -50, channel);
+  }
+
 }
 
 float de_sketchify(int port, int iter){
